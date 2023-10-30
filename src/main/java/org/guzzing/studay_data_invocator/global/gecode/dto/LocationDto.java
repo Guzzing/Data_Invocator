4727d3e1e4c7185a47986d43a7d5a12b43cf43c5
SeYoE <onetuks@naver.com>
2023-10-31 02:43:03 +0900

    Feat: Region 용 Geocoder 구현

---
+++ src/main/java/org/guzzing/studay_data_invocator/global/gecode/dto/LocationDto.java
@@ -0,0 +1,12 @@
+package org.guzzing.studay_data_invocator.global.gecode.dto;
+
+public record LocationDto(
+        String roadAddress,
+        String jibunAddress,
+        String englishAddress,
+        String x,
+        String y,
+        double distance
+) {
+
+}
