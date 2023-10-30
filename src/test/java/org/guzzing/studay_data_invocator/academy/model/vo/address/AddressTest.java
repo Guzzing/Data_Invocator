92454181574a8c67fefc4f2b46028b04be8a8126
byeolhaha <j35635aa@gmail.com>
2023-10-31 03:07:46 +0900

    Style : 불필요한 코드 삭제

--- src/test/java/org/guzzing/studay_data_invocator/academy/model/vo/address/AddressTest.java
+++ src/test/java/org/guzzing/studay_data_invocator/academy/model/vo/address/AddressTest.java
@@ -11,24 +11,6 @@ import org.junit.jupiter.api.Test;
 class AddressTest {
 
     @Test
-    @DisplayName("주소 정보 객체 생성에 성공한다.")
-    void instance_Address_Success() {
-        // Given
-        String dosi = "경기도";
-        String sigungu = "성남시";
-        String beopjungdong = "판교동";
-        String fullAddress = MessageFormat.format("{0} {1} {2} 대왕판교로 13323", dosi, sigungu, beopjungdong);
-
-        // When
-        Address result = Address.of(fullAddress);
-
-        // Then
-        assertThat(result.getSido()).isEqualTo(dosi);
-        assertThat(result.getSigungu()).isEqualTo(sigungu);
-        assertThat(result.getBeopjungdong()).isEqualTo(beopjungdong);
-    }
-
-    @Test
     @DisplayName("주소 정보 객체 생성에 실패한다.")
     void instance_address_Fail() {
         // Given
