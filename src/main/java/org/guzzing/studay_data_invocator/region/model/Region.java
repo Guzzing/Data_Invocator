9d9facd651d320d460792dc306629ea36ed8934c
SeYoE <onetuks@naver.com>
2023-10-30 22:52:24 +0900

    Feat: 지역 엔티티 추가

---
+++ src/main/java/org/guzzing/studay_data_invocator/region/model/Region.java
@@ -0,0 +1,37 @@
+package org.guzzing.studay_data_invocator.region.model;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.guzzing.studay_data_invocator.global.location.Address;
+import org.guzzing.studay_data_invocator.global.location.Location;
+
+@Entity
+@Table(name = "regions")
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Address address;
+
+    @Embedded
+    private Location location;
+
+    protected Region() {
+    }
+
+    protected Region(Address address, Location location) {
+        this.address = address;
+        this.location = location;
+    }
+
+    public static Region of(final Address address, final Location location) {
+        return new Region(address, location);
+    }
+}
