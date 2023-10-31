8bab4726cd630216939f97b1369135c760debb0c
byeolhaha <j35635aa@gmail.com>
2023-11-01 02:57:23 +0900

    Test : Invalid Academy 엔티티 삭제로 인해 테스트 코드 수정

--- src/test/java/org/guzzing/studay_data_invocator/academy/service/AcademyServiceTest.java
+++ src/test/java/org/guzzing/studay_data_invocator/academy/service/AcademyServiceTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-//@Transactional
 class AcademyServiceTest {
 
     @Autowired
