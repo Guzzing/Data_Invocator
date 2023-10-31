4a55dc702eb50fb54436bed1b23f90334eb9fd9c
byeolhaha <j35635aa@gmail.com>
2023-11-01 02:56:13 +0900

    Style : Invalid academy와 관련된 불필요한 파일 삭제

--- src/main/java/org/guzzing/studay_data_invocator/academy/repository/InvalidAcademyRepository.java
+++
@@ -1,8 +0,0 @@
-package org.guzzing.studay_data_invocator.academy.repository;
-
-import org.guzzing.studay_data_invocator.academy.model.InvalidAcademy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvalidAcademyRepository extends JpaRepository<InvalidAcademy, Long> {
-
-}
