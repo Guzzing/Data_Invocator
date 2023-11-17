59abba409471f5b3e710ddd7cdc7aba19aa797c9
wonu606 <wonu606@gmail.com>
2023-11-18 00:50:40 +0900

    fix: 공휴일이 없는 달도 받을 수 있도록 수정

--- src/main/java/org/guzzing/studay_data_invocator/holiday/resttemplate/KoreanHolidayResponse.java
+++ src/main/java/org/guzzing/studay_data_invocator/holiday/resttemplate/KoreanHolidayResponse.java
@@ -7,6 +7,7 @@ import jakarta.xml.bind.annotation.XmlElementWrapper;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -29,6 +30,9 @@ public class KoreanHolidayResponse {
         private List<Item> items;
 
         public List<Item> getItems() {
+            if (items == null) {
+                return new ArrayList<>();
+            }
             return items;
         }
 
