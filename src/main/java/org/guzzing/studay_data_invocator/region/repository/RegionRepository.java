2eddde9a8f70a282564756209eb709c475b4240c
byeolhaha <j35635aa@gmail.com>
2023-11-22 23:34:51 +0900

    Style : 불필요한 파일 삭제

--- src/main/java/org/guzzing/studay_data_invocator/region/repository/RegionRepository.java
+++
@@ -1,8 +0,0 @@
-package org.guzzing.studay_data_invocator.region.repository;
-
-import org.guzzing.studay_data_invocator.region.model.Region;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegionRepository extends JpaRepository<Region, Long> {
-
-}
