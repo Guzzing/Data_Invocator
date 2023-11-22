d5fc113686435e12ca8fa6a4eaec5e3e0fde9a08
byeolhaha <j35635aa@gmail.com>
2023-11-22 23:54:26 +0900

    Feat : 원본 학원 데이터 저장하는 레파지토리 구현

---
+++ src/main/java/org/guzzing/studay_data_invocator/sourceacademy/repository/SourceAcademyJpaRepository.java
@@ -0,0 +1,8 @@
+package org.guzzing.studay_data_invocator.sourceacademy.repository;
+
+import org.guzzing.studay_data_invocator.sourceacademy.model.SourceAcademy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SourceAcademyJpaRepository extends JpaRepository<SourceAcademy, Long> {
+
+}
