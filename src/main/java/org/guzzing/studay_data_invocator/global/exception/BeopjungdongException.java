92cdd4850092e0aaa3fc4817ab67f90fc4244265
SeYoE <onetuks@naver.com>
2023-11-24 04:25:09 +0900

    Remove: 불필요 클래스 제거

--- src/main/java/org/guzzing/studay_data_invocator/global/exception/BeopjungdongException.java
+++
@@ -1,8 +0,0 @@
-package org.guzzing.studay_data_invocator.global.exception;
-
-public class beopjeongdongException extends RuntimeException {
-
-    public beopjeongdongException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/guzzing/studay_data_invocator/region/parser/AreaDataParser.java
+++
@@ -1,8 +0,0 @@
-package org.guzzing.studay_data_invocator.region.parser;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class AreaDataParser {
-
-}
