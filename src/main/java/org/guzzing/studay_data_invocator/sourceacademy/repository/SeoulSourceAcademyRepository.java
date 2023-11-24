ab0b0c9a9c66f4fbb28b7c9b08f98612f260c7f7
byeolhaha <j35635aa@gmail.com>
2023-11-25 00:28:52 +0900

    Feat : 서울 원본 데이터 레파지토리 구현

---
+++ src/main/java/org/guzzing/studay_data_invocator/sourceacademy/repository/SeoulSourceAcademyRepository.java
@@ -0,0 +1,9 @@
+package org.guzzing.studay_data_invocator.sourceacademy.repository;
+
+
+import org.guzzing.studay_data_invocator.sourceacademy.model.SeoulSourceAcademy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeoulSourceAcademyRepository extends JpaRepository<SeoulSourceAcademy, Long> {
+
+}
