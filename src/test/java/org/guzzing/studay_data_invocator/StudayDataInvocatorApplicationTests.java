8b3fa366d01a9323cbaf967dfd5e151171679c93
SeYoE <onetuks@naver.com>
2023-11-25 17:44:02 +0900

    Feat: Region 데이터 파싱 실행기 등록

--- src/test/java/org/guzzing/studay_data_invocator/StudayDataInvocatorApplicationTests.java
+++ src/test/java/org/guzzing/studay_data_invocator/StudayDataInvocatorApplicationTests.java
@@ -1,6 +1,8 @@
 package org.guzzing.studay_data_invocator;
 
 import org.guzzing.studay_data_invocator.region.RegionDataInvocatorRunner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,5 +12,10 @@ class StudayDataInvocatorApplicationTests {
     @Autowired
     private RegionDataInvocatorRunner runner;
 
+    @Test
+    @DisplayName("법정동 데이터 파싱 실행")
+    void invocateData_Region() {
+        runner.invocateData();
+    }
 
 }
