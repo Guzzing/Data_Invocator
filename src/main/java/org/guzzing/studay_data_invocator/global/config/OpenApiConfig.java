b699eec7585413b060f564fb6dcebabbdcfd88e5
SeYoE <onetuks@naver.com>
2023-12-01 02:11:36 +0900

    Refactor: 파싱 설정 수정

--- src/main/java/org/guzzing/studay_data_invocator/global/config/OpenApiConfig.java
+++ src/main/java/org/guzzing/studay_data_invocator/global/config/OpenApiConfig.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Getter
 @Setter
@@ -11,7 +12,16 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(value = "openapi")
 public class OpenApiConfig {
 
-    private String apiUrl;
+    private String baseApiUrl;
     private String apiKey;
+
+    public String buildApiUrl(long admCode) {
+        return UriComponentsBuilder
+                .fromHttpUrl(baseApiUrl)
+                .queryParam("admCode", admCode)
+                .queryParam("key", apiKey)
+                .build()
+                .toUriString();
+    }
 
 }
