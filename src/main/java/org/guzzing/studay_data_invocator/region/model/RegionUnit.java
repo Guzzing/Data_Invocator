c13c9c5802e975a9c0ea48f0a5e47441debde23f
SeYoE <onetuks@naver.com>
2023-12-01 02:13:32 +0900

    Refactor: 읍면동 구분 추가

--- src/main/java/org/guzzing/studay_data_invocator/region/model/RegionUnit.java
+++ src/main/java/org/guzzing/studay_data_invocator/region/model/RegionUnit.java
@@ -5,7 +5,7 @@ import java.util.List;
 public enum RegionUnit {
     SIDO(List.of("시", "도")),
     SIGUNGU(List.of("시", "군", "구")),
-    UPMYEONDONG(List.of("구", "읍", "면", "동"));
+    UPMYEONDONG(List.of("구", "읍", "면", "동", "가", "로"));
 
     private final List<String> unitValue;
 
