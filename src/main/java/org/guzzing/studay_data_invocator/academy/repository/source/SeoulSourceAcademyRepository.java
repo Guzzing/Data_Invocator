fdb62a53523aa15df520b9e39b73edc2083a0bbb
SeYoE <onetuks@naver.com>
2023-12-01 14:34:18 +0900

    Remove: Region 모듈만 갖도록 제거

--- src/main/java/org/guzzing/studay_data_invocator/academy/repository/source/SeoulSourceAcademyRepository.java
+++
@@ -1,8 +0,0 @@
-package org.guzzing.studay_data_invocator.academy.repository.source;
-
-import org.guzzing.studay_data_invocator.academy.model.source.SeoulSourceAcademy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeoulSourceAcademyRepository extends JpaRepository<SeoulSourceAcademy, Long> {
-
-}
