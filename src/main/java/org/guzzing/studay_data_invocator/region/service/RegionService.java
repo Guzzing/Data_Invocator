c8518bb5212a7acca3f7d70b3291609649df8eb4
SeYoE <onetuks@naver.com>
2023-12-04 03:21:07 +0900

    Refactor: NameParameterJdbcTemplate 으로 마이그레이션

--- src/main/java/org/guzzing/studay_data_invocator/region/service/RegionService.java
+++ src/main/java/org/guzzing/studay_data_invocator/region/service/RegionService.java
@@ -1,7 +1,8 @@
 package org.guzzing.studay_data_invocator.region.service;
 
+import java.util.List;
 import org.guzzing.studay_data_invocator.region.model.Region;
-import org.guzzing.studay_data_invocator.region.repository.RegionJpaRepository;
+import org.guzzing.studay_data_invocator.region.repository.RegionRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,9 +10,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class RegionService {
 
-    private final RegionJpaRepository repository;
+    private final RegionRepository repository;
 
-    public RegionService(RegionJpaRepository repository) {
+    public RegionService(RegionRepository repository) {
         this.repository = repository;
     }
 
