36ade29410fe9e10c984d2d8e497140b2c8533dd
byeolhaha <j35635aa@gmail.com>
2024-01-12 19:13:54 +0900

    style : 파일명 수정

--- src/main/java/org/guzzing/studay_data_invocator/academy/category/maker/TutoringCategoryMaker.java
+++ src/main/java/org/guzzing/studay_data_invocator/academy/category/maker/TutoringCategoryMaker.java
@@ -1,6 +1,7 @@
 package org.guzzing.studay_data_invocator.academy.service.category.maker;
 
 import org.guzzing.studay_data_invocator.academy.model.source.GyeonggiSourceAcademy;
+import org.guzzing.studay_data_invocator.academy.model.source.SeoulSourceAcademy;
 import org.guzzing.studay_data_invocator.academy.service.category.AreaOfExpert;
 
 import java.util.List;
@@ -17,8 +18,20 @@ public class TutoringCategoryMaker implements CategoryMaker {
                 List.of(AreaOfExpert.TUTORING.getValue()), gyeonggiSourceAcademy.getAreaOfExpert())) {
             return true;
         }
+        if (CategoryFilter.isContains(
+                List.of("수학","과학","영어","국어"), gyeonggiSourceAcademy.getLessonCurriculum())) {
+            return true;
+        }
+        if (CategoryFilter.isContains(
+                List.of("보습"), gyeonggiSourceAcademy.getAcademyName())) {
+            return true;
+        }
 
         return false;
+    }
+
+    public boolean isContains(SeoulSourceAcademy seoulSourceAcademy) {
+        if
     }
 
 }
